13b840d05cc23672261bb499388b03e91e731825
{kkimjiyoon} <{bystar1687@nate.com}>
2024-02-23 16:45:34 +0900

    view

--- src/main/java/com/nhnacademy/edu/gateway/controller/MainController.java
+++ src/main/java/com/nhnacademy/edu/gateway/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.edu.gateway.controller;
 
-import com.nhnacademy.edu.gateway.request.LoginRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String goMain(Model model) {
-        return "main";
+        return "mainPage";
     }
 }
