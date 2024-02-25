73601e370747a9d62aa9ccd19cbe58ba2f8203cb
chaehyonNa <skcogus12@naver.com>
2024-02-25 22:22:47 +0900

    잠시

--- src/main/java/com/nhnacademy/edu/gateway/controller/TaskController.java
+++ src/main/java/com/nhnacademy/edu/gateway/controller/TaskController.java
@@ -1,12 +1,17 @@
 package com.nhnacademy.edu.gateway.controller;
 
 import com.nhnacademy.edu.gateway.adaptor.AccountAdaptor;
+import com.nhnacademy.edu.gateway.adaptor.ProjectAdaptor;
 import com.nhnacademy.edu.gateway.adaptor.ProjectMemberAdaptor;
+import com.nhnacademy.edu.gateway.adaptor.TaskAdaptor;
 import com.nhnacademy.edu.gateway.domain.Account;
+import com.nhnacademy.edu.gateway.domain.Project;
 import com.nhnacademy.edu.gateway.domain.ProjectMember;
+import com.nhnacademy.edu.gateway.domain.Task;
 import com.nhnacademy.edu.gateway.request.LoginRequest;
 import com.nhnacademy.edu.gateway.request.MemberRequest;
 import com.nhnacademy.edu.gateway.request.ProjectRequest;
+import com.nhnacademy.edu.gateway.request.TaskRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +25,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TaskController {
     private final AccountAdaptor accountAdaptor;
+
+    private final ProjectAdaptor projectAdaptor;
+
     private final ProjectMemberAdaptor projectMemberAdaptor;
+
+    private final TaskAdaptor taskAdaptor;
     @GetMapping("/task")
     public String taskMain(Model model) {
         List<Account> accountList = accountAdaptor.getAccounts();
@@ -31,7 +41,40 @@ public class TaskController {
     }
 
     @GetMapping("/taskDetail")
-    public String taskDetailTest() {
+    public String taskDetailTest(Model model, ProjectRequest projectRequest, HttpSession session) {
+        LoginRequest loginRequest = (LoginRequest) session.getAttribute("loginUserInfo");
+        Account account = accountAdaptor.getAccount(loginRequest.getUserId());
+        List<Project> projectList = projectAdaptor.getProjects(account.getUserName());
+        List<ProjectMember> projectMemberList = projectMemberAdaptor.getProjectMembers(projectRequest.getProjectNumber());
+        List<Task> taskList = taskAdaptor.getTasks(projectRequest.getProjectNumber());
+        List<Account> accountList = accountAdaptor.getAccounts();
+
+
+        model.addAttribute("accountList", accountList);
+        model.addAttribute("projectList",projectList);
+        model.addAttribute("accountInfo", account);
+        model.addAttribute("projectRequest",projectRequest);
+        model.addAttribute("projectMemberList",projectMemberList);
+        model.addAttribute("taskList",taskList);
+        return "taskDetailPage";
+    }
+
+    @PostMapping("/taskDetail/taskGet")
+    public String taskget(Model model, ProjectRequest projectRequest, TaskRequest taskRequest, HttpSession session) {
+        LoginRequest loginRequest = (LoginRequest) session.getAttribute("loginUserInfo");
+        Account account = accountAdaptor.getAccount(loginRequest.getUserId());
+        List<Project> projectList = projectAdaptor.getProjects(account.getUserName());
+        List<ProjectMember> projectMemberList = projectMemberAdaptor.getProjectMembers(projectRequest.getProjectNumber());
+        List<Task> taskList = taskAdaptor.getTasks(projectRequest.getProjectNumber());
+        List<Account> accountList = accountAdaptor.getAccounts();
+
+
+        model.addAttribute("accountList", accountList);
+        model.addAttribute("projectList",projectList);
+        model.addAttribute("accountInfo", account);
+        model.addAttribute("projectRequest",projectRequest);
+        model.addAttribute("projectMemberList",projectMemberList);
+        model.addAttribute("taskList",taskList);
         return "taskDetailPage";
     }
 
